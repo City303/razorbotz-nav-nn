ac5cb9bfb462f2d24e1d43d290ec3db597b909f6
City303 <City303@users.noreply.github.com>
2021-02-06 10:57:25 +0000

    Update to run script

--- nav_nn/test.py
+++ nav_nn/test.py
@@ -1,11 +1,5 @@
 import torch
+import yolov3
 print(torch)
 
 print(torch.cuda.is_available())
-
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
-fdir = 'https://github.com/ultralytics/yolov5/raw/master/data/images/'
-imgs = [fdir + f for f in ('zidane.jpg', 'bus.jpg')]  # batched list of images
-
-results = model(imgs)
-results.print()  # or .show(), .save()
